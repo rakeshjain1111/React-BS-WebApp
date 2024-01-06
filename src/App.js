9f1fa41e3f7b6f883f01c101009868f71d5878ba
rakeshjain1111 <rakeshkjain222@gmail.com>
2024-01-06 23:45:45 +0530

    project initilize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import  'bootstrap/dist/css/bootstrap.min.css';
+import  'bootstrap/dist/js/bootstrap.bundle';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path='/' element={<h1>Home Page</h1>}></Route>
+            <Route path='/blog' element={<h1>Blog Page</h1>}></Route>
+            <Route path='/ref' element={<h1>Reference Page</h1>}></Route>
+            <Route path='/learn' element={<h1>Learn Page</h1>}></Route>
+            <Route path='/contact' element={<h1>Contact Page</h1>}></Route>
+        </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
